f942f8eaabb474b8de1f7c51721bbc4b9ac3f31c
rgap <r.guzmanap@gmail.com>
2026-01-29 21:27:50 -0500

    chore: configure typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DATABASE_HOST || 'localhost',
+      port: parseInt(process.env.DATABASE_PORT || '5432'),
+      username: process.env.DATABASE_USER || 'postgres',
+      password: process.env.DATABASE_PASSWORD || 'root',
+      database: process.env.DATABASE_NAME || 'bd_back_nest_api',
+      entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+      synchronize: false, // set to false and then run "npm run typeorm migration:generate" to create the migration
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
